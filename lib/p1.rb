4514dc56cdd1064b415ae9954c66fe8369de6931
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-25 14:23:51 -0500

    Working multiple_of_3? method driven out by second test

---
+++ lib/p1.rb
@@ -0,0 +1,3 @@
+def multiple_of_3?(num)
+  num % 3 == 0
+end
--- spec/p1_spec.rb
+++ spec/p1_spec.rb
@@ -4,4 +4,8 @@ describe "#multiple_of_3?" do
   it "returns true for a multiple of 3" do
     expect(multiple_of_3?(276)).to be_true
   end
+
+  it "returns false for non-multiple" do
+    expect(multiple_of_3?(277)).to be_false
+  end
 end
