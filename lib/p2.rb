f3c8d17933ff883a7d4fcdefbc362d118a459d01
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-25 15:00:14 -0500

    Working solution to p2

---
+++ lib/p2.rb
@@ -0,0 +1,17 @@
+def sum_fibs(max)
+  prev = 1
+  curr = 1
+  sum = 0
+  until curr >= max
+    sum += curr if curr.even?
+    prev, curr = get_next_fib(prev, curr)
+  end
+
+  sum
+end
+
+def get_next_fib(prev, curr)
+  [curr, prev + curr]
+end
+
+puts sum_fibs(4_000_000)
