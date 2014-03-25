fe192a5db66dde7b7bee7130f3cd65e19c23eb06
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-25 14:44:57 -0500

    Working method and test cases

--- lib/p1.rb
+++ lib/p1.rb
@@ -1,3 +1,15 @@
+def sum_up_multiples_of_3_and_5(min, max)
+  (min..max).reduce(0) do |accum, num|
+    accum += if multiple_of_x?(3, num) || multiple_of_x?(5, num)
+      num
+    else
+      0
+    end
+  end
+end
+
 def multiple_of_x?(x, num)
   num % x == 0
 end
+
+puts sum_up_multiples_of_3_and_5(1, 999)
--- spec/p1_spec.rb
+++ spec/p1_spec.rb
@@ -21,3 +21,13 @@ describe "#multiple_of_x?" do
     expect(multiple_of_x?(x, 276)).to be_false
   end
 end
+
+describe "#sum_up_multiples_of_3_and_5" do
+  it 'works for numbers below 10' do
+    expect(sum_up_multiples_of_3_and_5(1, 9)).to eq 23
+  end
+
+  it 'works for numbers below 30' do
+    expect(sum_up_multiples_of_3_and_5(1, 29)).to eq 195
+  end
+end
