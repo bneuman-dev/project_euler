f83f88978105f6ba920a1b30d586c1514e3c1075
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-26 10:35:23 -0500

    #highest_palindrome still passes the test testing every single 3 digit * 3 digit possibility

--- lib/p4.rb
+++ lib/p4.rb
@@ -1,7 +1,7 @@
 def highest_palindrome
   palindromes = []
-  999.downto(900) do |num1|
-    num1.downto(900) do |num2|
+  999.downto(100) do |num1|
+    num1.downto(100) do |num2|
       product = num1 * num2
       palindromes << product if is_palindrome?(product.to_s)
     end
