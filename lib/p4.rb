b95f04af5fb8c6e9d61c85f9034586620c89e7e5
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-26 10:24:06 -0500

    Factor out head/tail check

--- lib/p4.rb
+++ lib/p4.rb
@@ -1,11 +1,15 @@
 def is_palindrome?(string, char_pointer=1)
   if char_pointer >= string.length / 2
-    return string[char_pointer - 1] == string[-(char_pointer)]
+    return head_and_tail_equal?(string, char_pointer)
   else
-    if string[char_pointer - 1] == string[-(char_pointer)]
+    if head_and_tail_equal?(string, char_pointer)
       is_palindrome?(string, char_pointer + 1)
     else
       return false
     end
   end
+end
+
+def head_and_tail_equal?(string, char_pointer)
+  string[char_pointer - 1] == string[-(char_pointer)]
 end
