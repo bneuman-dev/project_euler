fa642b62e8b192feec4c8d7d8c458a9d3059fadd
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-26 10:55:27 -0500

    Test for Euler case took waaaay too long. Refactored routine...took 16.1 seconds still

--- lib/p5.rb
+++ lib/p5.rb
@@ -1,7 +1,12 @@
 def lowest_divisible_by_all_of_range(min, max)
-  (max..max.factorial).each do |product|
-    return product if (min..max).all? { |num| product % num == 0 } 
+  num = max
+  until num.divisible_by_all_of_range?(min, max)
+    num += max
   end
+  num
+  # (max..max.factorial).each do |product|
+  #   return product if product.divisible_by_all_of_range?(min, max)
+  # end
 end
 
 class Fixnum
@@ -12,5 +17,9 @@ class Fixnum
   def factorial
     return 0 if self <= 0
     self.downto(1).reduce(:*)
+  end
+
+  def divisible_by_all_of_range?(min, max)
+    (min..max).all? { |num| has_factor?(num) } 
   end
 end
--- spec/p5_spec.rb
+++ spec/p5_spec.rb
@@ -5,4 +5,8 @@ describe "#lowest_divisible_by_all_of_range" do
   it "works for example case" do
     expect(lowest_divisible_by_all_of_range(1, 10)).to eq 2520
   end
+
+  it "works for 1-20 case" do
+    expect(lowest_divisible_by_all_of_range(1, 20)).to eq 232792560
+  end
 end
