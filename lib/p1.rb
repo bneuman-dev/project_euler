6ffe7309288d4eef126913af4023a7d922662d40
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-26 09:30:59 -0500

    Compulsive refactoring on p1...

--- lib/p1.rb
+++ lib/p1.rb
@@ -1,9 +1,13 @@
 def sum_up_multiples_of_3_and_5(min, max)
   sum = 0
   (min..max).each do |num| 
-    sum += num if multiple_of_x?(3, num) || multiple_of_x?(5, num)
+    sum += num if number_to_add?(num)
   end
   sum
+end
+
+def number_to_add?(num)
+  multiple_of_x?(3, num) || multiple_of_x?(5, num)
 end
 
 def multiple_of_x?(x, num)
