548071b39d745d834aa36370a607cd107484ab0c
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-26 09:44:25 -0500

    Added test to check it works with large numbers

--- spec/p3_spec.rb
+++ spec/p3_spec.rb
@@ -13,8 +13,12 @@ describe "#largest_prime_factor" do
   it "returns 5 for 5" do
     expect(largest_prime_factor(5)).to eq 5
   end
-  
+
   it "returns 3 for 9" do
     expect(largest_prime_factor(9)).to eq 3
+  end
+
+  it "returns correctly for a high number with obscure prime factors" do
+    expect(largest_prime_factor(10345739)).to eq 6793
   end
 end
